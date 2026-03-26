8787ac1deb0e7fe1b2918cb885ba3420d9223b87
Silver6819 <mr6874823@gmail.com>
2026-03-26 21:07:27 +0000

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "ok"}
+base_dir = os.path.dirname(os.path.realpath(__file__))
+templates = Jinja2Templates(directory=os.path.join(base_dir, "templates"))
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
