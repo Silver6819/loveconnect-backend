5eb71fffde100641ff9867135e4a1b7155041d4e
Silver6819 <mr6874823@gmail.com>
2026-03-26 14:53:55 -0600

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"status": "ok"}
 @app.get("/", response_class=HTMLResponse)
 async def home():
     return "<h1>LoveConnect funcionando 🚀</h1>"
